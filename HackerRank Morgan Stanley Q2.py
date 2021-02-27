488b26c74c5f1661015563db9d74d41fc401ced0
sicsempatyrannis <62498869+sicsempatyrannis@users.noreply.github.com>
2021-02-27 13:50:04 +0000

    Stock selection
    Given current savings, current value of a stock and future value of a stock what is the maximum profit that can be made.
    This sort of DS algorithm is used when recommeding portfolios to individuals based on the amount of money they have.

---
+++ HackerRank Morgan Stanley Q2.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 00:13:37 2020
+
+@author: 44797
+"""
+
+def selectStock(saving, currentValue, futureValue):
+    mem = {}
+    for i, j in enumerate(currentValue):
+        mem[j] = futureValue[i] - j
+    
+    global_max = 0
+    
+    for i in currentValue:
+        bank = saving
+        seen = []
+        values = currentValue.copy()
+        values.remove(i)
+        bank -= i
+        stack = sorted([j for j in values if j <= bank])
+        stack_1 = []
+        temp_max = mem[i]
+        
+        while stack:
+            top = stack.pop()
+
+            if top > bank and top not in seen:
+                add = stack_1.pop()
+                bank += add
+                temp_max -= mem[add]
+                stack.append(top)
+                
+            else:
+                bank -= top
+                seen.append(top)
+                stack_1.append(top)
+                temp_max += mem[top]
+        
+        global_max = max(global_max, temp_max)
+        
+    return global_max
+                
+            
+        
+                
+            
+            
+            
+print(selectStock(250, [175,133,109,210,97, 24], [200,125,128,228,133, 30]))
+            
+    
+   
+
+    
+    
+    
