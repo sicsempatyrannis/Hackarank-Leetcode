969380c136b46f5ad962c78785606582645294ff
sicsempatyrannis <62498869+sicsempatyrannis@users.noreply.github.com>
2021-03-22 13:41:47 +0000

    DFS both iterative and recursive solutions

--- HackerRank Morgan Stanley Q2.py
+++ HackerRank Morgan Stanley Q2.py
@@ -4,7 +4,7 @@ Created on Thu Nov 19 00:13:37 2020
 
 @author: 44797
 """
-
+## Iterative solution
 def selectStock(saving, currentValue, futureValue):
     mem = {}
     for i, j in enumerate(currentValue):
@@ -38,8 +38,49 @@ def selectStock(saving, currentValue, futureValue):
                 temp_max += mem[top]
                
     return global_max
-    
+
 print(selectStock(250, [175,133,109,210], [200,125,128,228]))
+
+## Recursive solution
+class Solution:
+    def __init__(self):
+        self.global_max = 0
+        self.visited = None
+        
+    def selectStock(self, saving, currentValue, futureValue):
+        self.current = currentValue
+       
+        mem = {}
+        for i, j in enumerate(currentValue):
+            mem[j] = futureValue[i] - j
+      
+        self.visited = [0] * len(currentValue)
+        
+        def dfs(i, bank, profit):
+            
+            if bank < self.current[i]:
+                self.global_max = max(profit, self.global_max)
+                return
+            
+            bank -= self.current[i]
+            profit += mem[self.current[i]]
+            self.visited[i] = -1
+            
+            for m, n in enumerate(currentValue):
+                if self.visited[m] != -1:
+                    dfs(m, bank, profit)
+                    
+            self.visited[i] = 1
+            
+        for v, k in enumerate(currentValue):
+            if v <= saving:
+                dfs(v, saving, 0)
+                
+        return self.global_max
+            
+print(Solution().selectStock(250, [175,133,109,210], [200,125,128,228])) 
+    
+
 
     
     
